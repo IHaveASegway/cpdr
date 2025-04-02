06dd75c8662842fa85466d3af29b0fe9e5ff4f24
Joseph Peterson <joseph.peterson@goaptive.com>
2025-04-02 15:21:57 -0600

    test

--- cpdr.rb
+++ cpdr.rb
@@ -1,10 +1,10 @@
 class Cpdr < Formula
     desc "CLI tool to copy directories recursively"
-    homepage "https://github.com/IHaveASegway/cpdr"
-    version "1.0.3"
-    url "https://github.com/ihaveasegway/cpdr/releases/download/v1.0.3/cpdr-1.0.3.tar.gz" # updated URL
-    sha256 "dd2d1e2dd08d9e05ec4823ca1ea9fe3030affbfec838e64ca7948d4965620d3b" # Update with the actual SHA256 hash for v1.0.3
+    homepage "https://github.com/ihaveasegway/cpdr"
+    url "https://github.com/ihaveasegway/cpdr/releases/download/v1.0.4/cpdr-1.0.4.tar.gz"
+    sha256 "fed790b56a8961041534f3ff7d7e8e2e948ebfff592b386e317e2ad13932736b"
     license "MIT"
+    version "1.0.4"
   
     depends_on "go" => :build
   
@@ -16,4 +16,4 @@ class Cpdr < Formula
     test do
       assert_match "cpdr", shell_output("#{bin}/cpdr --help 2>&1", 1)
     end
-end
+  end
